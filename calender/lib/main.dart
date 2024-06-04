fa9bd2da61eea11efd19094ba42ea5e6905057c2
14ZB990 <wkdqjatn1214@gmail.com>
2024-06-04 15:40:16 +0900

    First push

---
+++ calender/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:food_diary/models/diary_model.dart';
+import 'package:food_diary/pages/diary_home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => DiaryModel(),
+      child: MaterialApp(
+        home: DiaryHomePage(),
+      ),
+    );
+  }
+}
